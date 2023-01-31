5bacef4614e1ebf697530c73b872dd25cf973fae
mopin01 <carklimek@gmail.com>
2023-01-31 14:31:34 -0600

    Added discount method and tests for method

--- cart_discount/price_discount.py
+++ cart_discount/price_discount.py
@@ -9,8 +9,17 @@ def discount(item_prices):
     If a customer has ordered more than three items, the cheapest item is free.
     Example: if this function is called with a list of [10, 4, 20] then return 4.
     """
-
-    pass  # todo replace this line with your code 
+    try:
+        item_prices.sort()
+        if len(item_prices) >= 3:
+            if item_prices[0] >= 0:
+                return item_prices[0]
+            else:
+                raise Exception('Prices must not be negative.')
+        else:
+            return 0
+    except:
+        raise Exception('Enter a valid array of number prices')
 
 
 if __name__ == '__main__':
--- cart_discount/test_discount.py
+++ cart_discount/test_discount.py
@@ -1,17 +1,58 @@
-import unittest 
+import unittest
 from unittest import TestCase
-from price_discount import discount  
+from price_discount import discount
 
 class TestDiscount(TestCase):
 
     def test_list_of_three_prices(self):
-        prices = [10, 4, 20]
-        expected_discount = 4
+        prices = [58, 32, 90]
+        expected_discount = 32
         self.assertEqual(expected_discount, discount(prices))
 
-    
-    # TODO more unit tests here. Each test should test one scenario
+    def test_list_of_four_prices(self):
+        prices = [93, 35, 43, 6]
+        expected_discount = 6
+        self.assertEqual(expected_discount, discount(prices))
 
+    def test_list_of_two_prices(self):
+        prices = [32, 9]
+        expected_discount = 0
+        self.assertEqual(expected_discount, discount(prices))
+
+    def test_list_of_zero_prices(self):
+        prices = []
+        expected_discount = 0
+        self.assertEqual(expected_discount, discount(prices))
+    
+    def test_list_of_six_prices(self):
+        prices = [67, 67, 50, 35, 17, 93]
+        expected_discount = 17
+        self.assertEqual(expected_discount, discount(prices))
+
+    def test_string_prices(self):
+        prices = ['ten', 'four', 'twenty']
+        with self.assertRaises(Exception):
+            discount(prices)
+
+    def test_negative_prices(self):
+        prices = [-36, 4, 15]
+        with self.assertRaises(Exception):
+            discount(prices)
+
+    def test_floating_point_prices(self):
+        prices = [32.5, 72.2, 7.8]
+        expected_discount = 7.8
+        self.assertEqual(expected_discount, discount(prices))
+
+    def test_duplicate_prices(self):
+        prices = [10, 10, 20]
+        expected_discount = 10
+        self.assertEqual(expected_discount, discount(prices))
+    
+    def test_not_array(self):
+        prices = 10
+        with self.assertRaises(Exception):
+            discount(prices)
 
 if __name__ == '__main__':
     unittest.main()
